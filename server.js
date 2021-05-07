9bcb46c2ed551d9713ccce5dd5f89e5231f4b5d3
MUHAMMET KOMUR <muhammet075@icloud.com>
2021-05-08 01:28:34 +0200

    nieuw concept, voorkant gemaakt

--- server.js
+++ server.js
@@ -18,12 +18,12 @@ app.get('', (req, res) => {
     res.render('index')
 })
 
-app.get('/login', (req, res) => {
-    res.render('login')
+app.get('/aanmelden', (req, res) => {
+    res.render('aanmelden')
 })
 
-app.get('/register', (req, res) => {
-    res.render('register')
+app.get('/zoeken', (req, res) => {
+    res.render('zoeken')
 })
 
 app.listen(port, () => {
