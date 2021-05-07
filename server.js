25259f84a0f960ebf48d71c9d4ff29e32ab5a592
MUHAMMET KOMUR <muhammet075@icloud.com>
2021-05-07 03:46:10 +0200

    login, registren pagina aangemaakt template engine

--- server.js
+++ server.js
@@ -1,32 +1,31 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+
 const app = express();
 const port = 3000;
 
 
-app.get("/", home);
-app.get("/about", about);
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public.css'));
+app.use('/img', express.static(__dirname + 'public.img'));
 
 
-app.use(express.static('static'));
+app.use(expressLayouts);
+app.set('layout', './layouts/full-width')
 app.set("view engine", "ejs");
 
+app.get('', (req, res) => {
+    res.render('index')
+})
 
-function home(req, res){
-    res.render("index", {data: autos});
-}
+app.get('/login', (req, res) => {
+    res.render('login')
+})
 
-function about(req, res){
-    res.send("Over ons")
-}
-
+app.get('/register', (req, res) => {
+    res.render('register')
+})
 
 app.listen(port, () => {
     console.log("Server is aan");
 }) 
-
-
-
-const autos = {
-    merk: "Mercedes-Benz",
-    kleur: "Zwart"
-}
