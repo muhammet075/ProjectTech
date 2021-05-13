6571fe56644751af52a786c437da68e73d0677d8
MUHAMMET KOMUR <muhammet075@icloud.com>
2021-05-13 04:39:35 +0200

    data verwijderen uit db, aos.js, css aanpassingen

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
-
 const app = express();
 const port = 3000;
 
@@ -21,10 +20,10 @@ app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
- mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  runValidators: true,
+  mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    runValidators: true,
 })
 
 const gebruikerSchema = new mongoose.Schema({
@@ -33,6 +32,7 @@ const gebruikerSchema = new mongoose.Schema({
     },
     email: {
       type: String,
+      unique: true,
     },
     telefoon: {
       type: Number,
@@ -52,13 +52,19 @@ const gebruikerSchema = new mongoose.Schema({
     game4: {
       type: String,
     }
-  });
+});
 
 const gebruiker = mongoose.model("gebruiker", gebruikerSchema)
+
+
+
 
 app.get('', (req, res) => {
     res.render('index')
 })
+
+
+
 
 app.get('/aanmelden', (req, res) => {
     res.render('aanmelden')
@@ -75,9 +81,17 @@ app.post('/aanmelden', async (req, res) => {
         game3: req.body.game3,
         game4: req.body.game4
     });
-    nieuwGebruiker.save();
-    res.redirect('/zoeken');
+   
+    nieuwGebruiker.save(function (err) {
+      if (err) {
+        console.log('error');
+        res.redirect('/error')
+      } else {
+      res.redirect('/zoeken')
+      }
+    })
 })
+
 
 
 
@@ -89,21 +103,19 @@ app.get('/zoeken', (req, res) => {
   })
 })
 
+// nog in ontwikkeling
 app.post('/zoeken', async (req, res) => {
-
   const consoleFilter = req.body.consolefilter
-
   let query = {}
-
   if (consoleFilter === 'Alle') {
   } else {
     query = {
        console: req.body.consoleFilter
     }
     console.log(consoleFilter);
-  } 
-
+  }
 })
+
 
 
 
@@ -112,35 +124,70 @@ app.get('/wijzigen', (req, res) => {
 })
 
 app.post('/wijzigen', async (req, res) => {
-try {
-  const doc = await gebruiker.findOne({ email: req.body.wijzigemail });
-  doc.overwrite({     
-    naam: req.body.wijzignaam,
-    email: req.body.wijzigemail,
-    telefoon: req.body.wijzigtelefoon,
-    console: req.body.wijzigconsole,
-    game1: req.body.wijziggame1,
-    game2: req.body.wijziggame2,
-    game3: req.body.wijziggame3,
-    game4: req.body.wijziggame4 
-  });
+  try {
+    const doc = await gebruiker.findOne({ email: req.body.wijzigemail });
+      doc.overwrite({     
+        naam: req.body.wijzignaam,
+        email: req.body.wijzigemail,
+        telefoon: req.body.wijzigtelefoon,
+        console: req.body.wijzigconsole,
+        game1: req.body.wijziggame1,
+        game2: req.body.wijziggame2,
+        game3: req.body.wijziggame3,
+        game4: req.body.wijziggame4 
+      });
 
-  await doc.save();
+    await doc.save();
+    res.redirect('/zoeken');
 
-res.redirect('/zoeken');
-} catch(err) {
+  } catch(err) {
   console.log("Fout");
   res.redirect('/error');
-}
+  }
 })
+
+
+
+
+app.get('/verwijderen', (req, res) => {
+  res.render('verwijderen')
+})
+
+app.post('/verwijderen', async (req, res) => {
+  try {
+    await gebruiker.deleteMany({
+      email: req.body.verwijderemail,
+  })
+
+  res.redirect('/zoeken')
+
+  } catch (err) {
+    res.redirect('/error')
+  } 
+})
+
+
+
+
+app.get('/hoe-werkt-het', (req, res) => {
+  res.render('hoewerkthet')
+})
+
+
+
 
 app.get('/error', (req, res) => {
   res.render('error')
 })
 
+
+
+
 app.use(function(req, res) {
   res.status(404).render("404");
 });
+
+
 
 
 app.listen(port, () => {
