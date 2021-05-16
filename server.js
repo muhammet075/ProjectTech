c3e3961306ac959fdfd87355748cf3f6b6888719
MUHAMMET KOMUR <muhammet075@icloud.com>
2021-05-16 05:03:44 +0200

    code consistenter gemaakt, opgeschoond

--- server.js
+++ server.js
@@ -1,34 +1,34 @@
 //.env bestand aanroepen
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
 const app = express();
 const port = 3000;
 
 //de css, img en js map in de public map gebruiken
-app.use(express.static('public'));
-app.use('/css', express.static(__dirname + 'public.css'));
-app.use('/img', express.static(__dirname + 'public.img'));
-app.use('/js', express.static(__dirname + 'public.js'));
+app.use(express.static("public"));
+app.use("/css", express.static(__dirname + "public.css"));
+app.use("/img", express.static(__dirname + "public.img"));
+app.use("/js", express.static(__dirname + "public.js"));
 
 //express layout mobiel formaat en ejs gebruiken
 app.use(expressLayouts);
-app.set('layout', './layouts/mobiel-formaat')
+app.set("layout", "./layouts/mobiel-formaat");
 app.set("view engine", "ejs");
 
 //bodparser en express.json voor http requests
-var bodyParser = require('body-parser');
 app.use(express.json());
+var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
 //afbeeldingen uploaden
-var multer = require('multer');
+var multer = require("multer");
 
 //database connectie
-const mongoose = require('mongoose');
-const { request } = require('express');
+const mongoose = require("mongoose");
+const { request } = require("express");
 
 //db variable in de .env bestand aanroepen
   mongoose.connect(process.env.MONGO_URI, {
@@ -80,7 +80,7 @@ const gebruiker = mongoose.model("gebruiker", gebruikerSchema)
 //afbeeldingen worden opgeslagen in de public/uploads map
 const storage = multer.diskStorage({
   destination: function (request, file, callback){
-    callback(null, './public/uploads');
+    callback(null, "./public/uploads");
   },
 
 //afbeeldingen krijgen naast de oorspronkelijke naam ook de huidige datum
@@ -93,7 +93,7 @@ const storage = multer.diskStorage({
 //gewijzigde afbeeldingen
 const storageWijzig = multer.diskStorage({
   destination: function (request, file, callback){
-    callback(null, './public/uploads');
+    callback(null, "./public/uploads");
   },
 
   filename: function (request, file, callback){
@@ -121,17 +121,17 @@ const uploadWijzig = multer({
 
 //de app begint met de index.ejs bestand
 app.get('', (req, res) => {
-    res.render('index')
+    res.render("index");
 })
 
 
 //aanmelden route
-app.get('/aanmelden', (req, res) => {
-    res.render('aanmelden')
+app.get("/aanmelden", (req, res) => {
+    res.render("aanmelden");
 })
 
 //als er een nieuwe oproep geplaatst wordt, wordt de variabel gebruiker gevuld
-app.post('/aanmelden', upload.single('image'), async (req, res) => {
+app.post("/aanmelden", upload.single("image"), async (req, res) => {
   console.log(request.file)
     let nieuwGebruiker = new gebruiker({
         naam: req.body.naam,
@@ -150,32 +150,32 @@ app.post('/aanmelden', upload.single('image'), async (req, res) => {
     //de gevulde variable wordt opgeslagen als er een error is wordt de gebruiker doorverwezen naar de error pagina
     nieuwGebruiker.save(function (err) {
       if (err) {
-        console.log('error');
-        res.redirect('/error')
+        console.log("error");
+        res.redirect("/error");
       } else {
-      res.redirect('/zoeken')
+      res.redirect("/zoeken");
       }
     })
 })
 
 
 //zoeken route en gebruikers/oproepen in database vinden en mee sturen naar zoeken pagina
-app.get('/zoeken', (req, res) => {
+app.get("/zoeken", (req, res) => {
   gebruiker.find({}, function(err, gebruikers) {
-      res.render('zoeken', {
+      res.render("zoeken", {
           gebruikersLijst: gebruikers
       })
   })
 })
 
 //filter optie
-app.post('/zoeken', async (req, res) => {
+app.post("/zoeken", async (req, res) => {
 
   const consoleFilter = req.body.consolefilter
   //lege query voor als alle aangevingt is
   let query = {}
 
-  if (consoleFilter === 'Alle') {
+  if (consoleFilter === "Alle") {
     query = {}
 
   //query met de gekozen fitler optie uit de dropdown in de filter menu 
@@ -189,7 +189,7 @@ app.post('/zoeken', async (req, res) => {
   const gebruikers = await gebruiker.find(query).lean()
 
   //gebruikerslijst sturen en de filter optie
-  res.render('zoeken', {
+  res.render("zoeken", {
     gebruikersLijst: gebruikers,
     consoleFilter
   })
@@ -197,12 +197,12 @@ app.post('/zoeken', async (req, res) => {
 })
 
 //wijzigen route
-app.get('/wijzigen', (req, res) => {
-  res.render('wijzigen')
+app.get("/wijzigen", (req, res) => {
+  res.render("wijzigen");
 })
 
 //wijzigingen doorvoeren 
-app.post('/wijzigen', uploadWijzig.single('wijzigimage'), async (req, res) => {
+app.post("/wijzigen", uploadWijzig.single("wijzigimage"), async (req, res) => {
   try {
     //zoeken naar de juiste gebruiker aan de hand van de email die de gebruiker invoert
     const doc = await gebruiker.findOne({ email: req.body.wijzigemail });
@@ -222,45 +222,45 @@ app.post('/wijzigen', uploadWijzig.single('wijzigimage'), async (req, res) => {
 
     //de updates worden opgeslagen
     await doc.save();
-    res.redirect('/zoeken');
+    res.redirect("/zoeken");
 
   //bij een error wordt de gebruiker doorverwezen naar de error pagina
   } catch(err) {
   console.log(err);
-  res.redirect('/error');
+  res.redirect("/error");
   }
 })
 
 
 //verwijderen route
-app.get('/verwijderen', (req, res) => {
-  res.render('verwijderen')
+app.get("/verwijderen", (req, res) => {
+  res.render("verwijderen")
 })
 
-app.post('/verwijderen', async (req, res) => {
+app.post("/verwijderen", async (req, res) => {
   try {
     //met deletemany worden alle records van de object verwijderd, aan de hand van de email
     await gebruiker.deleteMany({
       email: req.body.verwijderemail,
   })
 
-  res.redirect('/zoeken')
+  res.redirect("/zoeken");
 
   } catch (err) {
-    res.redirect('/error')
+    res.redirect("/error");
   } 
 })
 
 
 //tutorial route
-app.get('/hoe-werkt-het', (req, res) => {
-  res.render('hoewerkthet')
+app.get("/hoe-werkt-het", (req, res) => {
+  res.render("hoewerkthet")
 })
 
 
 //error route
-app.get('/error', (req, res) => {
-  res.render('error')
+app.get("/error", (req, res) => {
+  res.render("error")
 })
 
 
